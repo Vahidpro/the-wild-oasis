9f0bc8830a8a1892fe33f2f9d1d0cbc004b52cb4
Vahidpro <ivahidp@gmail.com>
2024-04-22 17:00:36 +0330

    Added sidebar and main navigation menu

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,13 +1,24 @@
 import styled from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
 
 const StyledSideBar = styled.aside`
 	background-color: var(--color-gray-0) blue;
 	padding: 3.2rem 2.4rem;
 	border-bottom: 1px solid var(--color-gray-100);
 	grid-row: 1/-1;
+
+	display: flex;
+	flex-direction: column;
+	gap: 3.2rem;
 `;
 function SideBar() {
-	return <StyledSideBar>Sidebar</StyledSideBar>;
+	return (
+		<StyledSideBar>
+			<Logo />
+			<MainNav></MainNav>
+		</StyledSideBar>
+	);
 }
 
 export default SideBar;
