a92023862cd054c5fb2d6a6cac1500e323dbfb56
Vahidpro <ivahidp@gmail.com>
2024-04-22 15:55:39 +0330

    Defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,63 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-	padding: 20px;
-`;
 
 function App() {
 	return (
 		<>
 			<GlobalStyles />
-			<StyledApp>
-				<Row>
-					<Row type="horizontal">
-						<Heading as="h1">The Wild Oasis</Heading>
-						<div>
-							<Heading as="h2">Check in and out</Heading>
-							<Button
-								variation="primary"
-								size="medium"
-							>
-								Check in
-							</Button>
-							<Button
-								variation="secondary"
-								size="small"
-							>
-								Check out
-							</Button>
-						</div>
-					</Row>
-					<Row>
-						<form>
-							<Input placeholder="Number of guests" />
-							<Input placeholder="Number of guests" />
-						</form>
-					</Row>
-				</Row>
-			</StyledApp>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						index
+						element={
+							<Navigate
+								replace
+								to="dashboard"
+							/>
+						}
+					></Route>
+					<Route
+						path="dashboard"
+						element={<Dashboard />}
+					></Route>
+					<Route
+						path="bookings"
+						element={<Bookings />}
+					></Route>
+					<Route
+						path="cabins"
+						element={<Cabins />}
+					></Route>
+					<Route
+						path="users"
+						element={<NewUsers />}
+					></Route>
+					<Route
+						path="settings"
+						element={<Settings />}
+					></Route>
+					<Route
+						path="account"
+						element={<Account />}
+					></Route>
+					<Route
+						path="login"
+						element={<Login />}
+					></Route>
+					<Route
+						path="*"
+						element={<PageNotFound />}
+					></Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
